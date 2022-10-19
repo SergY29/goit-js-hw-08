3fe90b4e44e5ff5a381df5bad847fde0f1f52109
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-10-19 21:01:02 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,10 +8,14 @@ const player = new Player(iframe);
 player.on('timeupdate', throttle(timePlay, 1000));
 
 function timePlay({ seconds }) {
+
     localStorage.setItem("videoplayer-current-time", seconds)
 };
 
 const writing = localStorage.getItem('videoplayer-current-time');
 
-player.setCurrentTime(writing);
+if (writing) {
+    player.setCurrentTime(writing);
+};
+
 
